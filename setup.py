91344091003cf2ebec9e243c6acffc25f5ee3307
Alexandra Smith <alex@stitchdata.com>
2017-12-19 10:05:00 -0500

    bump 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.1.5",
+      version="0.1.6",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
