2bdcf7d2d50589ef1a74211621e21a8ad00939c6
Mike DeLaurentis <miked@stitchdata.com>
2017-12-26 15:45:07 -0500

    Version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.1.6",
+      version="0.1.7",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
