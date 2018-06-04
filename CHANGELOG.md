7aed176baae360fc3de6f6489506f0fa010fed54
Kyle Allan <kyle@stitchdata.com>
2018-06-04 13:16:08 -0400

    bump to 0.2.2

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 0.2.2
+  * Fixes a pagination bug that would cause Issue and Versions to return more pages than necessary [#14](https://github.com/singer-io/tap-jira/pull/14)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.2.1",
+      version="0.2.2",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
