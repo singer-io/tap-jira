2557afa4aa7356091c7febc63981d9ff5a12c2f8
Dan Mosora <dan@stitchdata.com>
2018-06-13 11:52:41 -0400

    Patch version bump and changelog update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.2.3
+  * Added `jsdPublic` field to `ticket_comments` schema.
+
 ## 0.2.2
   * Fixes a pagination bug that would cause Issue and Versions to return more pages than necessary [#14](https://github.com/singer-io/tap-jira/pull/14)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.2.2",
+      version="0.2.3",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
