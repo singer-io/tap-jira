820fe4d680c764996f405c5839e9eee58f15976c
Tim Visher <tim@stitchdata.com>
2018-08-31 19:35:27 +0000

    0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.3.0",
+      version="0.3.1",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
