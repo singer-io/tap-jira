1ce4b29de521656bb39ad0b813ca0d0b87a16b1f
Andy Lu <andy@stitchdata.com>
2018-12-10 17:01:50 +0000

    Create spike report

--- spikes/issues-fields-envelope-unwrapping/looking_at_issues_and_fields.py
+++ spikes/issues-fields-envelope-unwrapping/looking_at_issues_and_fields.py
@@ -13,7 +13,6 @@ REFRESH_TOKEN_EXPIRATION_PERIOD = 3500
 # > our past experience has shown that issuing queries no more than once every
 # > 10ms can help avoid performance issues
 TIME_BETWEEN_REQUESTS = timedelta(microseconds=10e3)
-
 REQUIRED_CONFIG_KEYS = ["start_date", "access_token"]
 
 
@@ -113,6 +112,11 @@ def setup_context():
     Context.state = args.state
 
 
+def write_header(title):
+    print('\n\n#----------------------------------------#')
+    print(title)
+    print('#----------------------------------------#')
+
 def spike_on(fields, issues):
     # Given the responses to API calls to the `fields` and `issues`
     # endpoints, where the `fields` call was specifically for `issue-id==
@@ -126,19 +130,17 @@ def spike_on(fields, issues):
 
     # The following should be output to a file in './tap_files' and to
     # STDOUT
-    #
-    # TODO: A raw `issues` object
-    # TODO: A raw `fields` object
-    # TODO: Proof that bubbling up fields from `fields` leads to name
-    #       collisions
-    # TODO: Proof that "name sanitizing" is a complex task
-    # TODO: Minimize the amount of tampering with the data
-    
+
     fields_schemas = []
-    fields_names = []
-    fields_ids = []
-    droppable_fields_fields = set()
+    fields_names = [field.get('name','NO_NAME_FOUND') for field in fields]
+    fields_ids = [field.get('id','NO_ID_FOUND') for field in fields]
+
+    fields_id_to_name = {f.get('id','NOT_FOUND'):f.get('name','NOT_FOUND') for f in fields}
+    processed_issue_nested_fields = {fields_id_to_name[k]:v for k,v in issues['fields'].items()}
+
     problem_resp_items = []
+    issue_fields = list(issues.keys())
+    issue_nested_fields = list(issues['fields'].keys())
 
     for field in fields:
         # Get fields of interest
@@ -148,22 +150,35 @@ def spike_on(fields, issues):
             fields_schemas.append('NO_SCHEMA_FOUND')
             problem_resp_items.append(field)
             continue
-        fields_names.append(field.get('name','NO_NAME_FOUND'))
-        fields_ids.append(field.get('id','NO_ID_FOUND'))
 
-        # Save all other fields
-        for trash in field.keys():
-            if trash not in ['id','name','schema']:
-                droppable_fields_fields.add(trash)
+    write_header("Here is a raw `issues` object")
+    print(issues)
 
-        issues_keys = list(issues.keys())
-        issues_fields_keys = list(issues['fields'].keys())
+    write_header("Here are some raw `fields` object: one standard, one custom")
+    print(fields[0])
+    print(fields[1])
 
-    
+    write_header("Here are the top level keys from `issues`")
+    print(issue_fields)
+
+    write_header("Here are the keys from the `issues` envelope")
+    print(issue_nested_fields)
+
+    write_header("The following are the keys found in `issues`'s top level fields and envelope")
+    found_in_both = [f for f in processed_issue_nested_fields if f in issue_fields]
+    print(found_in_both)
+
+    write_header('Here are strings we would need to sanitize (`fields`["name"])')
+    print(fields_names)
+
+    write_header('Here are the values of the `schema` key of `fields` objects')
+    print(fields_schemas)
+
+
 def main():
-    
+
     setup_context()
-    
+
     try:
         establish_client()
 
@@ -175,7 +190,6 @@ def main():
     finally:
         if Context.client and Context.client.login_timer:
             Context.client.login_timer.cancel()
-        print('Spike Completed!')
 
 
 if __name__ == '__main__':
