575eec91e781d4f2253fba85de02645328d0aa56
Andy Lu <andy@stitchdata.com>
2019-01-08 19:46:23 +0000

    Version 0.3.3 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="0.3.3",
+      version="1.0.0",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
