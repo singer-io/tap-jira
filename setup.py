6ff485817988b16a5ae108050e24c0a4b102dff5
KrishnanG <kgurusamy@talend.com>
2022-05-10 14:38:46 +0000

    Bump version for Jira

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="2.1.1",
+      version="2.1.2",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
