079aa2f9a1368130c7a28f937d2d03f73a8657af
Sourabh Gandhi <sgandhi@talend.com>
2025-11-04 21:35:07 +0000

    fallback to older endpoint if the /jql endpoint fails for the on prem instance

--- tap_jira/streams.py
+++ tap_jira/streams.py
@@ -6,7 +6,7 @@ import dateparser
 from singer import metrics, utils, metadata, Transformer
 from singer.transform import SchemaMismatch
 from dateutil.parser._parser import ParserError
-from .http import Paginator,JiraNotFoundError,IssuesPaginator
+from .http import Client,Paginator,JiraNotFoundError,IssuesPaginator
 from .context import Context
 
 DEFAULT_PAGE_SIZE = 50
@@ -317,10 +317,24 @@ class Issues(Stream):
                   "validateQuery": "strict",
                   "jql": jql}
         page_num = Context.bookmark(page_num_offset) or 0
-        pager = IssuesPaginator(Context.client, items_key="issues", page_num=page_num)
-        for page in pager.pages(self.tap_stream_id,
-                                "GET", "/rest/api/2/search/jql",
-                                params=params):
+        endpoint = "/rest/api/2/search/jql"
+        # Step 1: Validate which endpoint works
+        try:
+            Context.client.request(tap_stream_id=self.tap_stream_id, method="GET", path=endpoint, params=params)
+            issues_pages = pager.pages(self.tap_stream_id,
+                                "GET", endpoint,
+                                params=params)
+        except JiraNotFoundError as ex:
+            if "HTTP-error-code: 404" in str(ex) or "resource you have specified cannot be found" in str(ex).lower():
+                LOGGER.warning(
+                "Endpoint /rest/api/2/search/jql not supported on this JIRA instance. " \
+                "Falling back to /rest/api/2/search."
+                )
+            pager = Paginator(Context.client, items_key="issues", page_num=page_num)
+            issues_pages = pager.pages(self.tap_stream_id,
+                                "GET", "/rest/api/2/search",
+                                params=params)
+        for page in issues_pages:
             # sync comments and changelogs for each issue
             sync_sub_streams(page)
             for issue in page:
